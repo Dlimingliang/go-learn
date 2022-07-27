b3b811e6b1b6c677ee0e1d5bd7b5a798592970bd
limingliangaiduyangyang <lml2dyy@gmail.com>
2022-07-27 22:04:32 +0800

    使用锁的方式，并发写入map

--- 11_goroutine_and_channel/put_int_to_slice/main.go
+++ 11_goroutine_and_channel/put_int_to_slice/main.go
@@ -13,11 +13,64 @@ var (
 	lock sync.Mutex
 )
 
+type Data struct {
+	data []int
+	ch   chan int
+}
+
+func (s *Data) Consumer() {
+	for i := range s.ch {
+		s.data = append(s.data, i)
+	}
+}
+
+func (s *Data) Producer(value int) {
+	s.ch <- value
+}
+
+func (s *Data) CloseChannel() {
+	close(s.ch)
+}
+
+func NewScheduleJob(size int, done func()) *Data {
+	s := &Data{
+		ch:   make(chan int, size),
+		data: make([]int, 0),
+	}
+
+	go func() {
+		s.Consumer()
+		done()
+	}()
+	return s
+}
+
+func addToSliceByChannel() {
+	var (
+		n  = 10000
+		wg sync.WaitGroup
+	)
+	c := make(chan struct{})
+	s := NewScheduleJob(n, func() { c <- struct{}{} })
+	wg.Add(n)
+	for i := 0; i < n; i++ {
+		go func(value int) {
+			defer wg.Done()
+			s.Producer(value)
+		}(i)
+	}
+	wg.Wait()
+	s.CloseChannel()
+	<-c
+	fmt.Println(len(s.data))
+}
+
 func main() {
 	start := time.Now()
 	defer fmt.Println("总耗时:", time.Since(start))
 	//addToSlice()
-	addToSliceWaitGroup()
+	//addToSliceWaitGroup()
+	addToSliceByChannel()
 }
 
 func addToSlice() {
