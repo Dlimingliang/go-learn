81de10bbee26ee76f1ea0d617b54acb804e1705f
limingliangaiduyangyang <lml2dyy@gmail.com>
2022-07-27 17:40:10 +0800

    修改corn的模式，并且改造

---
+++ 11_goroutine_and_channel/put_string_to_map/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+var strMap = map[int]byte{}
+var lockMap *LockMap
+
+type LockMap struct {
+	sync.RWMutex
+	Map map[int]byte
+}
+
+func (l *LockMap) WriteMap(key int, value byte) {
+	l.Lock()
+	l.Map[key] = value
+	l.Unlock()
+}
+
+func main() {
+	start := time.Now()
+	defer fmt.Println("总耗时:", time.Since(start).Nanoseconds())
+	str := "xjioaoijfnqlerlulijasdxjioaoijfnqlerlulijasdxjioaoijfnqlerlulijasdxjioaoijfnqlerlulijasdxjioaoijfnqlerlulijasdxjioaoijfnqlerlulijasdxjioaoijfnqlerlulijasdxjioaoijfnqlerlulijasdf"
+	//addToMap(str)
+	addToMapWaitGroup(str)
+
+}
+
+func addToMap(str string) {
+	for i := 0; i < len(str); i++ {
+		time.Sleep(100 * time.Millisecond)
+		strMap[i] = str[i]
+	}
+	fmt.Println(strMap)
+}
+
+func addToMapWaitGroup(str string) {
+	lockMap = &LockMap{
+		Map: make(map[int]byte),
+	}
+	var mg sync.WaitGroup
+	mg.Add(len(str))
+	for i := 0; i < len(str); i++ {
+		go func(index int, value byte) {
+			defer mg.Done()
+			time.Sleep(100 * time.Millisecond)
+			lockMap.WriteMap(index, value)
+		}(i, str[i])
+	}
+	mg.Wait()
+	fmt.Println(lockMap.Map)
+}
